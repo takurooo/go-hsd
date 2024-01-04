4aa7b7cb615f476c74d1661bcbdfcb2815a73fa7
takurooo <35373553+takurooo@users.noreply.github.com>
2024-01-04 20:52:39 +0900

    add go doc

--- hsd.go
+++ hsd.go
@@ -1,9 +1,16 @@
+// Package hsd provides functions to calculate the distance between strings.
 package hsd
 
+// StringDistance calculates the distance between two strings, lhs and rhs.
+// The distance is defined as the count of differing characters.
+// Returns -1 if the lengths of the strings are different.
 func StringDistance(lhs, rhs string) int {
 	return Distance([]rune(lhs), []rune(rhs))
 }
 
+// Distance calculates the distance between two rune slices, a and b.
+// The distance is defined as the count of differing runes.
+// Returns -1 if the lengths of the slices are different.
 func Distance(a, b []rune) int {
 	dist := 0
 	if len(a) != len(b) {
