d5dfe531b2c9bc9b1a137e233b498caef002cd19
takurooo <35373553+takurooo@users.noreply.github.com>
2024-01-04 21:58:51 +0900

    update test

--- hsd_test.go
+++ hsd_test.go
@@ -3,9 +3,21 @@ package hsd
 import "testing"
 
 func TestStringDistance(t *testing.T) {
-	got := StringDistance("foo", "foh")
-	want := 1
-	if got != want {
-		t.Fatalf("expected: %v, got: %v", want, got)
+	cases := []struct {
+		lhs      string
+		rhs      string
+		expected int
+	}{
+		{lhs: "abc", rhs: "abc", expected: 0},
+		{lhs: "abcd", rhs: "abef", expected: 2},
+		{lhs: "abcd", rhs: "cdef", expected: 4},
+		{lhs: "abc", rhs: "ab", expected: -1},
+	}
+
+	for _, c := range cases {
+		result := StringDistance(c.lhs, c.rhs)
+		if result != c.expected {
+			t.Fatalf("expected: %v, result: %v", c.expected, result)
+		}
 	}
 }
